feb11570efceca92b3ddac0658f23b77a84c65c9
SybileC <sybile.cherenfant@gmail.com>
2020-10-13 18:34:27 -0400

    Updated table schemata codes

--- table schemata.sql
+++ table schemata.sql
@@ -1,42 +1,62 @@
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS titles;
+
+
 CREATE TABLE titles(
-	title_id int,
-	title VARCHAR(255),
+	title_id VARCHAR NOT NULL,
+	title VARCHAR NOT NULL,
 	PRIMARY KEY(title_id)
 );
 
+SELECT * FROM titles;
+
 CREATE TABLE employees(
-	emp_no int,
-	emp_title int NOT NULL,
-	birth_date int NOT NULL,
-	first_name VARCHAR(255) NOT NULL,
-	last_name VARCHAR(255) NOT NULL,
+	emp_no INT NOT NULL,
+	emp_title VARCHAR NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
 	sex VARCHAR(1) NOT NULL,
-	hire_date int NOT NULL, 
+	hire_date DATE NOT NULL, 
 	PRIMARY KEY(emp_no),
 	FOREIGN KEY(emp_title) REFERENCES titles(title_id)
 					  );
 					  
+SELECT * FROM employees;
+
 CREATE TABLE departments(
-	dept_no int,
-	dept_name VARCHAR(255),
+	dept_no VARCHAR NOT NULL,
+	dept_name VARCHAR NOT NULL,
 	PRIMARY KEY(dept_no)
 );
 
+SELECT * FROM departments;
+
 CREATE TABLE dept_emp(
-	emp_no int NOT NULL,
-	dept_no int NOT NULL,
+	emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
 );
 
+SELECT * FROM dept_emp;
+
 CREATE TABLE dept_manager(
-	dept_no int NOT NULL,
-	emp_no int NOT NULL,
+	dept_no VARCHAR NOT NULL,
+	emp_no INT NOT NULL,
 	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY(emp_no) REFERENCES employees(emp_no)
 );
 
+SELECT * FROM dept_manager;
+
 CREATE TABLE salaries(
-	emp_no int NOT NULL, 
-	salaries int NOT NULL
+	emp_no INT NOT NULL, 
+	salaries INT NOT NULL
 );
+
+SELECT * FROM salaries;
